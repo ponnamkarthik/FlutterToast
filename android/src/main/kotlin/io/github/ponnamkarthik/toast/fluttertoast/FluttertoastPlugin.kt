eb33fb4c6f11e47c2ca4922be97dff7fecb852ed
Caijinglong <cjl_spy@163.com>
2018-07-24 16:57:08 +0800

    the android toast use single instance

--- android/src/main/kotlin/io/github/ponnamkarthik/toast/fluttertoast/FluttertoastPlugin.kt
+++ android/src/main/kotlin/io/github/ponnamkarthik/toast/fluttertoast/FluttertoastPlugin.kt
@@ -1,97 +1,100 @@
 package io.github.ponnamkarthik.toast.fluttertoast
 
 import android.content.Context
-import android.util.Log
-import android.view.Gravity
-import android.widget.Toast
 import android.graphics.Color
+import android.graphics.Paint
 import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RoundRectShape
+import android.view.Gravity
+import android.widget.TextView
+import android.widget.Toast
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
-import android.widget.TextView
-import android.graphics.Paint
-import android.graphics.drawable.ShapeDrawable
-import android.graphics.drawable.shapes.RoundRectShape
 
 
+class FluttertoastPlugin(context: Context) : MethodCallHandler {
+
+    var ctx: Context? = context
 
 
-
-class FluttertoastPlugin(context: Context): MethodCallHandler {
-
-  var ctx: Context? = context
-    var toast: Toast? = null
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar): Unit {
-      val channel = MethodChannel(registrar.messenger(), "PonnamKarthik/fluttertoast")
-
-      channel.setMethodCallHandler(FluttertoastPlugin(registrar.context()))
+    companion object {
+        var toast: Toast? = null
+        @JvmStatic
+        fun registerWith(registrar: Registrar): Unit {
+            val channel = MethodChannel(registrar.messenger(), "PonnamKarthik/fluttertoast")
+            channel.setMethodCallHandler(FluttertoastPlugin(registrar.context()))
+        }
     }
-  }
 
+    var defaultTextColor: Int = Color.TRANSPARENT
 
-  override fun onMethodCall(call: MethodCall, result: Result): Unit {
+    override fun onMethodCall(call: MethodCall, result: Result): Unit {
 
-    if (call.method.equals("showToast")) {
-        val msg: String = call.argument("msg")
-        val length: String = call.argument("length")
-        val gravity: String = call.argument("gravity")
-        val bgcolor: String = call.argument("bgcolor")
-        val textcolor: String = call.argument("textcolor")
+        if (call.method == "showToast") {
 
-        val toast: Toast = this.toast?:Toast.makeText(ctx, msg, Toast.LENGTH_SHORT);
-        this.toast = toast
-        toast.setText(msg)
-      if(length.equals("long")) {
-          toast.duration = Toast.LENGTH_LONG
-      } else {
-          toast.duration = Toast.LENGTH_SHORT
-      }
+            val msg: String = call.argument("msg")
+            val length: String = call.argument("length")
+            val gravity: String = call.argument("gravity")
+            val bgcolor: String = call.argument("bgcolor")
+            val textcolor: String = call.argument("textcolor")
 
-        if(gravity.equals("top")) {
-            toast.setGravity(Gravity.TOP, 0, 100)
-        } else if(gravity.equals("center")) {
-            toast.setGravity(Gravity.CENTER, 0, 0)
+            val toast: Toast = FluttertoastPlugin.toast ?: Toast.makeText(ctx, msg, Toast.LENGTH_SHORT);
+            FluttertoastPlugin.toast = toast
+            toast.setText(msg)
+            if (length.equals("long")) {
+                toast.duration = Toast.LENGTH_LONG
+            } else {
+                toast.duration = Toast.LENGTH_SHORT
+            }
+
+            when (gravity) {
+                "top" -> toast.setGravity(Gravity.TOP, 0, 100)
+                "center" -> toast.setGravity(Gravity.CENTER, 0, 0)
+                else -> toast.setGravity(Gravity.BOTTOM, 0, 100)
+            }
+
+            val text: TextView = toast.view.findViewById(android.R.id.message)
+            if (defaultTextColor == 0) {
+                defaultTextColor = text.currentTextColor
+            }
+            if (bgcolor != "null") {
+
+                try {
+
+                    val rectShape = RoundRectShape(floatArrayOf(50f, 50f, 50f, 50f, 50f, 50f, 50f, 50f), null, null)
+
+                    val shapeDrawable = ShapeDrawable(rectShape)
+                    shapeDrawable.paint.color = Color.parseColor(bgcolor)
+                    shapeDrawable.paint.style = Paint.Style.FILL
+                    shapeDrawable.paint.isAntiAlias = true
+                    shapeDrawable.paint.flags = Paint.ANTI_ALIAS_FLAG
+
+                    text.setBackgroundDrawable(shapeDrawable)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            } else {
+                text.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            }
+            if (textcolor != "null") {
+                try {
+                    text.setTextColor(Color.parseColor(textcolor))
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            } else {
+                text.setTextColor(defaultTextColor)
+            }
+
+            toast.show()
+
+            result.success("Success")
         } else {
-            toast.setGravity(Gravity.BOTTOM, 0, 100)
+            result.notImplemented()
         }
-
-        val text: TextView = toast.view.findViewById(android.R.id.message)
-        if(bgcolor != "null") {
-
-            try {
-
-                val rectShape = RoundRectShape(floatArrayOf(50f, 50f, 50f, 50f, 50f, 50f, 50f, 50f), null, null)
-
-                val shapeDrawable = ShapeDrawable(rectShape)
-                shapeDrawable.paint.color = Color.parseColor(bgcolor)
-                shapeDrawable.paint.style = Paint.Style.FILL
-                shapeDrawable.paint.isAntiAlias = true
-                shapeDrawable.paint.flags = Paint.ANTI_ALIAS_FLAG
-
-                text.setBackgroundDrawable(shapeDrawable)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-        if(textcolor != "null") {
-            try {
-                text.setTextColor(Color.parseColor(textcolor))
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-
-        toast.show()
-
-      result.success("Success")
-    } else {
-      result.notImplemented()
     }
-  }
 }
