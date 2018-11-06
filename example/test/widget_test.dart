e2c5473372dc5a10bf582dabb31ddef2f3dafee3
Karthik Ponnam <ponnamkarthik3@gmail.com>
2018-11-06 11:15:47 +0530

    dry run meta fix

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -3,23 +3,6 @@
 // provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:fluttertoast_example/main.dart';
-
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
 
-    // Verify that platform version is retrieved.
-    expect(
-        find.byWidgetPredicate(
-          (Widget widget) =>
-              widget is Text && widget.data.startsWith('Running on:'),
-        ),
-        findsOneWidget);
-  });
 }
