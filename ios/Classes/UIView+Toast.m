f470178b0b24a287ae609f9462bba2514dcb302c
Karthik Ponnam <ponnamkarthik3@gmail.com>
2019-05-15 19:56:34 +0530

    hope ios notch fixed

--- ios/Classes/UIView+Toast.m
+++ ios/Classes/UIView+Toast.m
@@ -423,7 +423,7 @@ static const NSString * CSToastQueueKey             = @"CSToastQueueKey";
         if([point caseInsensitiveCompare:CSToastPositionTop] == NSOrderedSame) {
             return CGPointMake(self.bounds.size.width/2, (toast.frame.size.height / 2) + style.verticalPadding);
         } else if([point caseInsensitiveCompare:CSToastPositionCenter] == NSOrderedSame) {
-            return CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2);
+            return CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2) + style.verticalPadding;
         }
     } else if ([point isKindOfClass:[NSValue class]]) {
         return [point CGPointValue];
@@ -448,7 +448,7 @@ static const NSString * CSToastQueueKey             = @"CSToastQueueKey";
         self.maxWidthPercentage = 0.8;
         self.maxHeightPercentage = 0.8;
         self.horizontalPadding = 10.0;
-        self.verticalPadding = 10.0;
+        self.verticalPadding = 20.0;
         self.cornerRadius = 10.0;
         self.titleFont = [UIFont boldSystemFontOfSize:16.0];
         self.messageFont = [UIFont systemFontOfSize:16.0];
