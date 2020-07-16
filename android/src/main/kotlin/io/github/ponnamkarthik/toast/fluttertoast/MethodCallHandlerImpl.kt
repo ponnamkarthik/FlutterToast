cab825b8ae7839e08a3a70af35b50e0a6c0ad15c
panmin <panmin@panmindeMacBook-Pro.local>
2020-07-16 19:06:47 +0800

    4

--- android/src/main/kotlin/io/github/ponnamkarthik/toast/fluttertoast/MethodCallHandlerImpl.kt
+++ android/src/main/kotlin/io/github/ponnamkarthik/toast/fluttertoast/MethodCallHandlerImpl.kt
@@ -1,6 +1,7 @@
 package io.github.ponnamkarthik.toast.fluttertoast
 
 import android.app.Activity
+import android.app.Application
 import android.content.Context
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
