9f2a0e76de9e520cdd06ee793c7471cbc2ddd319
Yaakov Schectman <schectman@google.com>
2024-10-29 16:11:35 -0400

    Remove v1 embedding

--- android/src/main/kotlin/io/github/ponnamkarthik/toast/fluttertoast/FlutterToastPlugin.kt
+++ android/src/main/kotlin/io/github/ponnamkarthik/toast/fluttertoast/FlutterToastPlugin.kt
@@ -1,12 +1,9 @@
 package io.github.ponnamkarthik.toast.fluttertoast
 
 import android.content.Context
-import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
-import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** FlutterToastPlugin */
 public class FlutterToastPlugin: FlutterPlugin {
